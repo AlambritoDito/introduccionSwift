eda996ffc7245b12436b333d35c13a6b31197ee3
Brito <britoditodeveloper@gmail.com>
2021-12-23 14:29:20 -0600

    Se termino el archivo 02_var

--- 01_hello_world.playground/Contents.swift
+++ 01_hello_world.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+//Swift es un 'Type safe language' lo que significa que para definir una variable no necesito poner explicitamente el tipo
 var greeting = "Hello, playground"
 
 
---
+++ 02_var_let.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//Para constantes es let
+let maximumNumberOfLoginAttempts = 3
+//variable
+var currentLoginAttempt = 0
+//Podemos crear varias variables solo poniendo ','
+var x = 0.0, y = 0.0, z = 0.0
+//Esto sirve para definir el valor de una variable sin necesidad de poner un valor
+var welcomeMessage : String
+
+//Metodos de escribir variables: 'Snake Case' "no es el estandar es Swift"
+var test_test :String
+//'Camel Case'
+var testTest :String
+
+//option + p = π
+let π = 3.1416
+
+let 🤠 = "CowBoy"
+
+print(🤠)
+
+//Imprimir string y variables
+
+print("El valor de pi es: \(π) y el emoji que esta en codigo: \(🤠)")
+
+
